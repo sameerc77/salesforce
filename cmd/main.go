91dedf238f058e5fde4be0cea5fd4c6d4137de99
Sameer Chaudhari <sameer.c77@gmail.com>
2023-01-07 15:19:07 +0530

    log

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,9 @@ import (
 
 	"github.com/sameerc77/salesforce/connection"
 	"github.com/sameerc77/salesforce/db"
+	"github.com/sameerc77/salesforce/logger"
 )
 
 func main() {
-	fmt.Println(connection.GetSalesforceConn(), db.GetConnection(), logeer.Getlog())
+	fmt.Println(connection.GetSalesforceConn(), db.GetConnection(), logger.Getlog())
 }
